3f228d0f5ef121ca0428b58e53e2024c71d39e2b
siche <sichecmm@gmail.com>
2020-10-27 14:24:04 +0800

    update

--- Ion/image_processing.py
+++ Ion/image_processing.py
@@ -1,62 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import cv2
-
+import cv2, sys, win32gui, win32ui, win32con, win32api, PIL.Image
+from PyQt5.QtWidgets import QApplication
+from PyQt5 import QtGui
 # get screen shot by window name
-import win32gui, win32ui, win32con, win32api
-import sys
-
-hwnd_title = dict()
-def getAllWindow(hwnd, mouse):
-    if win32gui.IsWindow(hwnd) and win32gui.IsWindowEnabled(hwnd) and win32gui.IsWindowVisible(hwnd):
-        hwnd_title.update({hwnd: win32gui.GetWindowText(hwnd)})
-
-def getWindow(windowName = 'Andor'):
-    win32gui.EnumWindows(getAllWindow, 0)
-    wanted_title = -1
-    for h, t in hwnd_title.items():
-        if windowName in t:
-            return (h,t)
-
-    if wanted_title == -1:
-        win32api.MessageBox(0, "Andor Solis is not running", "Warning",win32con.MB_ICONWARNING)
-        sys.exit()
-    
-def window_capture(windowName='Andor'):
-    (win_num, win_title) = getWindow(windowName)
-    # 根据窗口句柄获取窗口的设备上下文DC（Divice Context）
-    hwndDC = win32gui.GetWindowDC(win_num)
-    # 根据窗口的DC获取mfcDC
-    mfcDC = win32ui.CreateDCFromHandle(hwndDC)
-    # mfcDC创建可兼容的DC
-    saveDC = mfcDC.CreateCompatibleDC()
-    # 创建bigmap准备保存图片
-    saveBitMap = win32ui.CreateBitmap()
-
-
-    # get physical monitor information
-    """
-    MoniterDev = win32api.EnumDisplayMonitors(None, None)
-    w = MoniterDev[0][2][2]
-    h = MoniterDev[0][2][3]
-    """
-
-    # but sometimes there is scaled in the real resolution
-    # there it's more important to get actual resolution
-
-    w = int(win32api.GetSystemMetrics(0)/2)
-    h = win32api.GetSystemMetrics(1)
-    # print w,h　　　#图片大小
-    # 为bitmap开辟空间
-    saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
-    # 高度saveDC，将截图保存到saveBitmap中
-    saveDC.SelectObject(saveBitMap)
-    # 截取从左上角（0，0）长宽为（w，h）的图片
-    saveDC.BitBlt((0, 0), (w, h), mfcDC, (0, 0), win32con.SRCCOPY)
-    # saveBitMap.SaveBitmapFile(saveDC, filename)
-    img = np.fromstring(saveBitMap.GetBitmapBits(True), dtype = np.uint8)
-    img = img.reshape((h,w,-1))
-    return cv2.cvtColor(img, cv2.COLOR_RGBA2GRAY)
 
 def bw_analysis(binary_img, p=10):
     img_size = np.shape(binary_img)
@@ -158,6 +105,122 @@ def bw_analysis(binary_img, p=10):
 
     return (binary_img, ion_num, ion_info)
 
+hwnd_title = dict()
+def getAllWindow(hwnd, mouse):
+    if win32gui.IsWindow(hwnd) and win32gui.IsWindowEnabled(hwnd) and win32gui.IsWindowVisible(hwnd):
+        hwnd_title.update({hwnd: win32gui.GetWindowText(hwnd)})
+
+def getWindow(windowName = 'Andor'):
+    win32gui.EnumWindows(getAllWindow, 0)
+    wanted_title = -1
+    for h, t in hwnd_title.items():
+        if windowName in t:
+            return (h,t)
+
+    if wanted_title == -1:
+        win32api.MessageBox(0, "Andor Solis is not running", "Warning",win32con.MB_ICONWARNING)
+        sys.exit()
+
+def window_capture(windowName='Andor'):
+    """
+    using native windows api to get capture
+    It's the fastest, about ~ms per screenshot   
+    """
+    (win_num, win_title) = getWindow(windowName)
+    # 根据窗口句柄获取窗口的设备上下文DC（Divice Context）
+    hwndDC = win32gui.GetWindowDC(win_num)
+    # 根据窗口的DC获取mfcDC
+    mfcDC = win32ui.CreateDCFromHandle(hwndDC)
+    # mfcDC创建可兼容的DC
+    saveDC = mfcDC.CreateCompatibleDC()
+    # 创建bigmap准备保存图片
+    saveBitMap = win32ui.CreateBitmap()
+
+
+    # get physical monitor information
+    """
+    MoniterDev = win32api.EnumDisplayMonitors(None, None)
+    w = MoniterDev[0][2][2]
+    h = MoniterDev[0][2][3]
+    """
+
+    # but sometimes there is scaled in the real resolution
+    # there it's more important to get actual resolution
+
+    w = int(win32api.GetSystemMetrics(0)/2)
+    h = win32api.GetSystemMetrics(1)
+    # print w,h　　　#图片大小
+    # 为bitmap开辟空间
+    saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
+    # 高度saveDC，将截图保存到saveBitmap中
+    saveDC.SelectObject(saveBitMap)
+    # 截取从左上角（0，0）长宽为（w，h）的图片
+    saveDC.BitBlt((0, 0), (w, h), mfcDC, (0, 0), win32con.SRCCOPY)
+    # saveBitMap.SaveBitmapFile(saveDC, filename)
+
+    # ! Using fromstring is much faster then normal method
+    img = np.fromstring(saveBitMap.GetBitmapBits(True), dtype = np.uint8)
+    img = img.reshape((h,w,-1))
+    return cv2.cvtColor(img, cv2.COLOR_RGBA2GRAY)
+
+
+"""
+pre method prepared for function windos_capture2()
+"""
+def qimage2numpy(qimage, dtype='array'):
+    """Convert QImage to numpy.ndarray.  The dtype defaults to uint8
+    for QImage.Format_Indexed8 or `bgra_dtype` (i.e. a record array)
+    for 32bit color images.  You can pass a different dtype to use, or
+    'array' to get a 3D uint8 array for color images."""
+    result_shape = (qimage.height(), qimage.width())
+    temp_shape = (qimage.height(),
+                  int(qimage.bytesPerLine() * 8 / qimage.depth()))
+    if qimage.format() in (QtGui.QImage.Format_ARGB32_Premultiplied,
+                           QtGui.QImage.Format_ARGB32,
+                           QtGui.QImage.Format_RGB32):
+        if dtype == 'rec':
+            dtype = QtGui.bgra_dtype
+        elif dtype == 'array':
+            dtype = np.uint8
+            result_shape += (4,)
+            temp_shape += (4,)
+    elif qimage.format() == QtGui.QImage.Format_Indexed8:
+        dtype = np.uint8
+    else:
+        raise ValueError("qimage2numpy only supports 32bit and 8bit images")
+        # FIXME: raise error if alignment does not match
+
+    buf = qimage.bits().asstring(qimage.byteCount())
+    result = np.frombuffer(buf, dtype).reshape(temp_shape)
+    if result_shape != temp_shape:
+        result = result[:, :result_shape[1]]
+    if qimage.format() == QtGui.QImage.Format_RGB32 and dtype == np.uint8:
+        result = result[..., :3]
+    result = result[:, :, ::-1] 
+    return result
+
+def rgb2gray(img):
+    """
+    convert rgb image to gray scale image
+    """
+    if len(img.shape) == 2:
+        return img
+    else:
+        cv2.cvtColor(img2, cv2.COLOR_RGB2GRAY)
+
+def window_capture2(windows_name = 'Andor'):
+    """
+    Backup Method 
+    Use qt to  get screenshot
+    a little slower than win32api but much more faster than other method 
+    like pyautogit etc.
+    """
+    (win_num, win_title) = getWindow(windowName)
+    hwnd = win32gui.FindWindow(None, win_title)
+    app = QApplication(sys.argv)
+    screen = QApplication.primaryScreen()
+    img = screen.grabWindow(hwnd).toImage()
+    return qimage2numpy(img)
 
 def has_ion(plt_option = False, bw_threshold = 160, ion_area = 15, region = [200,750,200,650]):
     img = window_capture()
